fe39b95886aec6f9946e67ddd509bb866c505888
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2024-07-12 16:24:35 +0200

    Tests for NameToken

--- casper-name-contracts/src/contracts/name_token.rs
+++ casper-name-contracts/src/contracts/name_token.rs
@@ -110,12 +110,10 @@ impl NameToken {
         );
     }
 
-    // TODO: Test this function.
     pub fn token_exists(&self, token_hash: &String) -> bool {
         self.token.token_exists_by_hash(token_hash)
     }
 
-    // TODO: Test this function, make sure only admin can call it.
     pub fn burn(&mut self, token_id: Maybe<u64>, token_hash: Maybe<String>) {
         let caller = self.env().caller();
         if !self.token.is_whitelisted(&caller) {
@@ -127,7 +125,6 @@ impl NameToken {
         self.token.burn_token_unchecked(token_id, caller);
     }
 
-    // TODO: Test this function.
     pub fn admin_transfer(&mut self, reciepient: Address, token_hashes: Vec<String>) {
         let spender = self.env().caller();
         if !self.token.is_whitelisted(&spender) {
@@ -140,7 +137,6 @@ impl NameToken {
         }
     }
 
-    // TODO: Test this function.
     pub fn set_token_metadata(
         &mut self,
         token_id: Maybe<u64>,
@@ -216,4 +212,285 @@ impl NameToken {
 pub enum NameTokenError {
     NotWhitelisted = 1301,
     InvalidTokenOwner = 1302,
+}
+
+#[cfg(test)]
+mod tests {
+    use odra::OdraResult;
+
+    use super::*;
+    use crate::test_context::{TestContext, INIT_TIME};
+
+    #[test]
+    fn test_token_exists() {
+        let mut ctx = TestContext::install_raw();
+        let token_hash = "token_hash";
+        // Token should not exist
+        assert_eq!(ctx.token.token_exists(&token_hash.to_string()), false);
+
+        // Mint a token
+        let token_owner = ctx.alice;
+        mint_for(&mut ctx, token_owner, token_hash);
+        // Then the token should exist
+        assert_eq!(ctx.token.token_exists(&token_hash.to_string()), true);
+
+        // Burn the token
+        whitelist_accounts(&mut ctx, vec![token_owner]);
+        ctx.set_caller(token_owner);
+        assert!(try_burn(&mut ctx, token_hash).is_ok());
+        // Then the token should not exist anymore
+        assert_eq!(ctx.token.token_exists(&token_hash.to_string()), false);
+    }
+
+    #[test]
+    fn owner_cannot_burn_until_whitelisted() {
+        let mut ctx = TestContext::install_raw();
+        // Given a token owned by alice
+        let token_hash = "token_hash";
+        let token_owner = ctx.alice;
+        mint_for(&mut ctx, token_owner, token_hash);
+
+        // Then burning token by the owner fails
+        ctx.set_caller(token_owner);
+        let result = try_burn(&mut ctx, token_hash);
+        assert_eq!(result.err(), Some(NameTokenError::NotWhitelisted.into()));
+
+        // When whitelist the token owner
+        whitelist_accounts(&mut ctx, vec![token_owner]);
+        // Then the token owner should be able to burn the token
+        ctx.set_caller(token_owner);
+        let result = try_burn(&mut ctx, token_hash);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn anyone_whitelisted_can_burn() {
+        let mut ctx = TestContext::install_raw();
+        let (token_owner, anyone) = (ctx.alice, ctx.anyone);
+        // Given a token owned by the token owner
+        let token_hash = "token_hash";
+        mint_for(&mut ctx, token_owner, token_hash);
+
+        // Then anyone should not be able to burn the token
+        ctx.set_caller(anyone);
+        let result = try_burn(&mut ctx, token_hash);
+        assert_eq!(result.err(), Some(NameTokenError::NotWhitelisted.into()));
+
+        // When whitelist the anyone account
+        whitelist_accounts(&mut ctx, vec![anyone]);
+        // Then the anyone account should be able to burn the token
+        ctx.set_caller(anyone);
+        let result = try_burn(&mut ctx, token_hash);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn burning_burnt_token_should_fail() {
+        let mut ctx = TestContext::install_raw();
+        // Given a token owned by alice
+        let alice = ctx.alice;
+        let token_hash = "token hash";
+        mint_for(&mut ctx, alice, token_hash);
+        // When whitelist the token owner
+        whitelist_accounts(&mut ctx, vec![alice]);
+        assert!(ctx.token.token_exists(&token_hash.to_string()));
+        ctx.set_caller(alice);
+        // Then the token owner should be able to burn the token
+        assert!(try_burn(&mut ctx, token_hash).is_ok());
+        // Then burning the token again should fail
+        assert!(try_burn(&mut ctx, token_hash).is_err());
+    }
+
+    #[test]
+    fn burning_non_existent_token_should_fail() {
+        let mut ctx = TestContext::install_raw();
+        let alice = ctx.alice;
+        // Given a non existent token
+        let token_hash = "token hash";
+        assert!(!ctx.token.token_exists(&token_hash.to_string()));
+
+        // Then burning the token should fail
+        whitelist_accounts(&mut ctx, vec![alice]);
+        ctx.set_caller(alice);
+        assert!(try_burn(&mut ctx, token_hash).is_err());
+    }
+
+    #[test]
+    fn admin_transfer_of_multiple_tokens_should_work() {
+        let mut ctx = TestContext::install_raw();
+        let (alice, bob) = (ctx.alice, ctx.bob);
+        // Given two tokens owned by alice
+        let token_hashes = vec!["token_hash1".to_string(), "token_hash2".to_string()];
+        mint_for(&mut ctx, alice, &token_hashes[0]);
+        mint_for(&mut ctx, alice, &token_hashes[1]);
+
+        // When admin transfer the tokens to bob
+        whitelist_accounts(&mut ctx, vec![alice]);
+        ctx.set_caller(alice);
+        ctx.token.admin_transfer(bob, token_hashes.clone());
+        // Then bob should be the owner of the tokens
+        assert!(ctx.token.try_assert_is_owner(&token_hashes[0], bob).is_ok());
+        assert!(ctx.token.try_assert_is_owner(&token_hashes[1], bob).is_ok());
+    }
+
+    #[test]
+    fn admin_transfer_of_multiple_tokens_should_not_work_for_non_whitelisted_account() {
+        let mut ctx = TestContext::install_raw();
+        let (installer, alice, bob) = (ctx.admin, ctx.alice, ctx.bob);
+        // Given two tokens owned by alice
+        let token_hashes = vec!["token_hash1".to_string(), "token_hash2".to_string()];
+        mint_for(&mut ctx, alice, &token_hashes[0]);
+        mint_for(&mut ctx, alice, &token_hashes[1]);
+
+        // When non whitelisted account tries to transfer the tokens
+        // (Neither the installer nor the alice account is whitelisted)
+        ctx.set_caller(alice);
+        assert_eq!(
+            ctx.token.try_admin_transfer(bob, token_hashes.clone()),
+            Err(NameTokenError::NotWhitelisted.into())
+        );
+
+        ctx.set_caller(installer);
+        assert_eq!(
+            ctx.token.try_admin_transfer(bob, token_hashes.clone()),
+            Err(NameTokenError::NotWhitelisted.into())
+        );
+
+        // Then bob should not be the owner of the tokens
+        assert!(ctx
+            .token
+            .try_assert_is_owner(&token_hashes[0], bob)
+            .is_err());
+        assert!(ctx
+            .token
+            .try_assert_is_owner(&token_hashes[1], bob)
+            .is_err());
+    }
+
+    #[test]
+    fn admin_transfer_of_non_existent_token_should_fail() {
+        let mut ctx = TestContext::install_raw();
+        let (alice, bob) = (ctx.alice, ctx.bob);
+        // Given two tokens owned by alice
+        let token_hashes = vec![
+            "token_hash1".to_string(),
+            "token_hash2".to_string(),
+            "token_hash3".to_string(),
+        ];
+        mint_for(&mut ctx, alice, &token_hashes[0]);
+        mint_for(&mut ctx, alice, &token_hashes[1]);
+
+        // When admin transfer the tokens to bob
+        whitelist_accounts(&mut ctx, vec![alice]);
+        ctx.set_caller(alice);
+        // Then the transfer fails
+        assert!(ctx
+            .token
+            .try_admin_transfer(bob, token_hashes.clone())
+            .is_err());
+        // Then the existing tokens should not be transferred
+        assert!(ctx
+            .token
+            .try_assert_is_owner(&token_hashes[0], bob)
+            .is_err());
+        assert!(ctx
+            .token
+            .try_assert_is_owner(&token_hashes[1], bob)
+            .is_err());
+    }
+
+    #[test]
+    fn only_owner_can_set_resolver() {
+        let mut ctx = TestContext::install_raw();
+        let (alice, bob) = (ctx.alice, ctx.bob);
+        // Given a token owned by alice
+        let token_hash = "token_hash";
+        mint_for(&mut ctx, alice, token_hash);
+        // Then the token has no resolver
+        assert_eq!(ctx.token.resolver(token_hash.to_owned()), None);
+
+        // When bob tries to set the resolver
+        let resolver = bob;
+        ctx.set_caller(bob);
+        // Then the operation should fail
+        assert_eq!(
+            ctx.token.try_set_resolver(token_hash.to_owned(), resolver),
+            Err(NameTokenError::InvalidTokenOwner.into())
+        );
+        // When alice sets the resolver
+        ctx.set_caller(alice);
+        ctx.token.set_resolver(token_hash.to_owned(), resolver);
+        // Then the resolver should be set
+        assert_eq!(ctx.token.resolver(token_hash.to_owned()), Some(resolver));
+    }
+
+    #[test]
+    fn test_is_token_valid() {
+        let mut ctx = TestContext::install_raw();
+        let alice = ctx.alice;
+
+        // Given a token with expiration time in furure
+        let token_hash = "token hash";
+        let expiration = INIT_TIME + 100;
+        ctx.set_caller(ctx.admin);
+        let token_meta_data = NameTokenMetadata::with_no_resolver(token_hash, expiration);
+        ctx.token.mint(
+            alice,
+            token_meta_data.to_json().unwrap(),
+            Maybe::Some(token_hash.to_owned()),
+        );
+        // Then the token should be valid
+        assert!(ctx.token.is_token_valid(&token_hash.to_string()));
+        // When the expiration time is passed
+        ctx.advance_block_time(expiration + 1);
+        // Then the token should not be valid
+        assert!(!ctx.token.is_token_valid(&token_hash.to_string()));
+    }
+
+    #[test]
+    fn burnt_token_is_invalid() {
+        let mut ctx = TestContext::install_raw();
+        let alice = ctx.alice;
+
+        // Given a token with expiration time in furure
+        let token_hash = "token hash";
+        let expiration = INIT_TIME + 100;
+        ctx.set_caller(ctx.admin);
+        let token_meta_data = NameTokenMetadata::with_no_resolver(token_hash, expiration);
+        ctx.token.mint(
+            alice,
+            token_meta_data.to_json().unwrap(),
+            Maybe::Some(token_hash.to_owned()),
+        );
+        // Then the token should be valid
+        assert!(ctx.token.is_token_valid(&token_hash.to_string()));
+
+        // When the token is burnt
+        whitelist_accounts(&mut ctx, vec![alice]);
+        ctx.set_caller(alice);
+        assert!(try_burn(&mut ctx, token_hash).is_ok());
+        // Then the token should not be valid
+        assert!(!ctx.token.is_token_valid(&token_hash.to_string()));
+    }
+
+    fn mint_for(ctx: &mut TestContext, owner: Address, token_hash: &str) {
+        ctx.set_caller(ctx.admin);
+        let token_meta_data = NameTokenMetadata::with_no_resolver(token_hash, 0);
+        ctx.token.mint(
+            owner,
+            token_meta_data.to_json().unwrap(),
+            Maybe::Some(token_hash.to_owned()),
+        );
+    }
+
+    fn whitelist_accounts(ctx: &mut TestContext, accounts: Vec<Address>) {
+        ctx.set_caller(ctx.admin);
+        ctx.token
+            .set_variables(Maybe::None, Maybe::Some(accounts), Maybe::None);
+    }
+
+    fn try_burn(ctx: &mut TestContext, token_hash: &str) -> OdraResult<()> {
+        ctx.token
+            .try_burn(Maybe::None, Maybe::Some(token_hash.to_owned()))
+    }
 }

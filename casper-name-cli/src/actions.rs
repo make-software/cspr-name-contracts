c309fdfa7e254c10968e4d59b8500dd8c06ef2c1
Krzysztof Pobiarżyn <krzysztof@odra.dev>
2024-07-30 11:52:24 +0200

    Add more commands to the cli

--- casper-name-cli/src/actions.rs
+++ casper-name-cli/src/actions.rs
@@ -1,18 +1,20 @@
-use std::str::FromStr;
-
+use blake2::digest::VariableOutput;
+use blake2::Blake2bVar;
 use casper_name_contracts::contracts::controller::{
     Controller, ControllerHostRef, ControllerInitArgs,
 };
 use casper_name_contracts::contracts::registrar::{
     Registrar, RegistrarHostRef, RegistrarInitArgs, CONTROLLER_ROLE,
 };
-use casper_name_contracts::contracts::resolver::{DefaultResolverHostRef, DefaultResolverInitArgs};
+use casper_name_contracts::contracts::resolver::{DefaultResolver, DefaultResolverHostRef, DefaultResolverInitArgs};
 use casper_name_contracts::data_structures::{NameMintInfo, PaymentVoucher, TokenizationVoucher};
 use odra::args::Maybe;
 use odra::casper_types::bytesrepr::{Bytes, ToBytes};
+use odra::casper_types::{AsymmetricType, PublicKey};
 use odra::contract_def::HasIdent;
-use odra::host::Deployer;
-use odra::host::HostRef;
+use odra::host::{Deployer, HostEnv, HostRef};
+use std::io::Write;
+use std::str::FromStr;
 
 use casper_name_contracts::contracts::name_token::{
     NameToken, NameTokenHostRef, NameTokenInitArgs,
@@ -25,10 +27,9 @@ pub const ONE_DAY: u64 = 86400000;
 pub const GRACE_PERIOD: u64 = ONE_DAY * 2;
 pub const EXPIRATION: u64 = ONE_DAY * 365;
 
-pub fn deploy_all() {
+pub fn deploy_all(env: &HostEnv) {
     DeployedContractsToml::handle_previous_version();
     let mut contracts = DeployedContractsToml::new();
-    let env = odra_casper_livenet_env::env();
     let admin = env.get_account(0);
 
     env.set_gas(300_000_000_000);
@@ -41,13 +42,21 @@ pub fn deploy_all() {
     );
     contracts.add_contract(&NameToken::ident(), token.address());
 
+    env.set_gas(200_000_000_000);
+    let resolver = DefaultResolverHostRef::deploy(
+        &env,
+        DefaultResolverInitArgs {
+            name_token: *token.address(),
+        },
+    );
+    contracts.add_contract(&DefaultResolver::ident(), resolver.address());
+
     env.set_gas(150_000_000_000);
     let registrar = RegistrarHostRef::deploy(
         &env,
         RegistrarInitArgs {
             name_token: *token.address(),
-            // TODO: replace with a resolver address
-            default_resolver: *token.address(),
+            default_resolver: *resolver.address(),
         },
     );
     contracts.add_contract(&Registrar::ident(), registrar.address());
@@ -62,26 +71,16 @@ pub fn deploy_all() {
         },
     );
     contracts.add_contract(&Controller::ident(), controller.address());
-
-    env.set_gas(200_000_000_000);
-    let resolver = DefaultResolverHostRef::deploy(
-        &env,
-        DefaultResolverInitArgs {
-            name_token: *token.address(),
-        },
-    );
-    contracts.add_contract(&DefaultResolverHostRef::ident(), resolver.address());
 }
 
-pub fn set_config() {
-    let env = odra_casper_livenet_env::env();
+pub fn set_config(env: &HostEnv) {
     let mut contracts = DeployedContracts::load(&env);
 
     // Whitelist the registrar in the name token.
     env.set_gas(1_000_000_000);
     contracts.token.set_variables(
         Maybe::Some(true),
-        Maybe::Some(vec![contracts.registrar.address().clone()]),
+        Maybe::Some(vec![*contracts.registrar.address()]),
         Maybe::None,
     );
 
@@ -102,44 +101,216 @@ pub fn set_config() {
         .set_default_resolver(*contracts.resolver.address());
 }
 
-pub fn registrar_register(name: &str, buyer: &str) {
-    let env = odra_casper_livenet_env::env();
-    let mut contracts = DeployedContracts::load(&env);
+pub mod registrar {
+    use super::*;
+    use casper_name_contracts::data_structures::{RenewalVoucher, TokenRenewalInfo};
 
-    let owner = Address::from_str(buyer).unwrap();
-    let now: u64 = chrono::Utc::now().timestamp_millis() as u64;
-    let token_expiration = now + EXPIRATION;
-    let voucher_expiration = now + ONE_DAY;
-    let names = vec![NameMintInfo::new(name, owner, token_expiration)];
-    let voucher = TokenizationVoucher::new(names, voucher_expiration);
+    pub fn register(env: &HostEnv, name: String, buyer: String, token_validity: Option<u64>) {
+        let owner = parse_address(&buyer);
+        let token_expiration = now() + token_validity.unwrap_or(EXPIRATION);
+        let voucher_expiration = now() + ONE_DAY;
+        let names = vec![NameMintInfo::new(&name, owner, token_expiration)];
+        let voucher = TokenizationVoucher::new(names, voucher_expiration);
 
-    env.set_gas(10_000_000_000);
-    contracts.registrar.register(voucher);
+        env.set_gas(10_000_000_000);
+        contract(env).register(voucher);
+    }
+
+    pub fn prolong(env: &HostEnv, name: String, token_validity: Option<u64>) {
+        let token_expiration = now() + token_validity.unwrap_or(EXPIRATION);
+        let voucher_expiration = now() + ONE_DAY;
+        let names = vec![TokenRenewalInfo::new(name, token_expiration)];
+        let voucher = RenewalVoucher::new(names, voucher_expiration);
+
+        env.set_gas(10_000_000_000);
+        contract(env).prolong(voucher);
+    }
+
+    pub fn set_default_resolver(env: &HostEnv, address: String) {
+        let resolver = parse_address(&address);
+        assert!(resolver.is_contract());
+
+        env.set_gas(3_000_000_000);
+        contract(env).set_default_resolver(resolver);
+    }
+
+    pub fn set_grace_period(env: &HostEnv, seconds: u64) {
+        env.set_gas(3_000_000_000);
+        contract(env).set_grace_period(seconds);
+    }
+
+    pub fn resolve(env: &HostEnv, domain: String) {
+        env.set_gas(10_000_000_000);
+        contract(env).resolve(domain);
+    }
+
+    fn contract(env: &HostEnv) -> RegistrarHostRef {
+        let contracts = DeployedContracts::load(env);
+        contracts.registrar
+    }
 }
 
-pub fn controller_buy(name: &str) {
-    let env = odra_casper_livenet_env::env();
-    let contracts = DeployedContracts::load(&env);
-    let admin = env.get_account(0);
+pub mod controller {
+    use casper_name_contracts::data_structures::{RenewalPaymentVoucher, TokenRenewalInfo};
 
-    let now: u64 = chrono::Utc::now().timestamp_millis() as u64;
-    let token_expiration = now + EXPIRATION;
-    let voucher_expiration = token_expiration;
-    let amount = 1_000_000_000.into();
-    let payment_id = "pay_001";
-    let names = vec![NameMintInfo::new(
-        name,
-        env.get_account(0),
-        voucher_expiration,
-    )];
+    use super::*;
 
-    let payment_voucher = PaymentVoucher::new(amount, &payment_id, admin, names, token_expiration);
-    let bytes: Bytes = payment_voucher.to_bytes().unwrap().into();
-    let signature = env.sign_message(&bytes, &admin);
+    pub fn buy(env: &HostEnv, name: String, token_validity: Option<u64>) {
+        let admin = env.get_account(0);
 
-    env.set_gas(50_000_000_000);
-    contracts
-        .controller
-        .with_tokens(amount)
-        .buy(payment_voucher, signature);
+        let token_expiration = now() + token_validity.unwrap_or(EXPIRATION);
+        let voucher_expiration = token_expiration;
+        let amount = 1_000_000_000.into();
+        let payment_id = "pay_001";
+        let names = vec![NameMintInfo::new(&name, admin, voucher_expiration)];
+
+        let payment_voucher =
+            PaymentVoucher::new(amount, &payment_id, admin, names, token_expiration);
+        let bytes: Bytes = payment_voucher.to_bytes().unwrap().into();
+        let signature = env.sign_message(&bytes, &admin);
+
+        env.set_gas(5_000_000_000);
+        contract(env)
+            .with_tokens(amount)
+            .buy(payment_voucher, signature);
+    }
+
+    pub fn renew(env: &HostEnv, name: String, token_validity: Option<u64>) {
+        let admin = env.get_account(0);
+
+        let token_expiration = now() + token_validity.unwrap_or(EXPIRATION);
+        let voucher_expiration = token_expiration;
+        let amount = 1_000_000_000.into();
+        let payment_id = "pay_001";
+        let names = vec![TokenRenewalInfo::new(name, voucher_expiration)];
+
+        let payment_voucher =
+            RenewalPaymentVoucher::new(amount, &payment_id, admin, names, token_expiration);
+        let bytes: Bytes = payment_voucher.to_bytes().unwrap().into();
+        let signature = env.sign_message(&bytes, &admin);
+
+        contract(env)
+            .with_tokens(1_000_000_000.into())
+            .renew(payment_voucher, signature)
+    }
+
+    pub fn resolve(env: &HostEnv, domain: String) {
+        let result = contract(env).resolve(domain);
+
+        let result = result
+            .map(|addr| addr.to_string())
+            .unwrap_or_else(|| "Not found".to_string());
+        prettycli::info(&result);
+    }
+
+    fn contract(env: &HostEnv) -> ControllerHostRef {
+        let contracts = DeployedContracts::load(env);
+        contracts.controller
+    }
+}
+
+pub mod token {
+    use super::*;
+
+    pub fn set_resolver(env: &HostEnv, token_name: String, address: String) {
+        let resolver = parse_address(&address);
+        assert!(resolver.is_contract());
+
+        let token_id = blake2b(&token_name);
+        env.set_gas(3_000_000_000);
+        contract(env).set_resolver(token_id, resolver);
+    }
+
+    pub fn resolver(env: &HostEnv, token_name: String) {
+        let token_id = blake2b(&token_name);
+        let result = contract(env).resolver(token_id);
+
+        let result = result
+            .map(|addr| addr.to_string())
+            .unwrap_or_else(|| "Not found".to_string());
+
+        let message = format!("Resolver for {:?}: {:?}", token_name, result);
+        prettycli::info(&message);
+    }
+
+    pub fn metadata(env: &HostEnv, token_name: String) {
+        let token_id = blake2b(&token_name);
+        let result = contract(env).try_metadata_by_hash(&token_id);
+
+        let result = result
+            .map(|metadata| serde_json::to_string_pretty(&metadata).ok())
+            .ok()
+            .flatten()
+            .unwrap_or_else(|| "Not found".to_string());
+
+        let message = format!("Metadata for {:?}:\n{}", token_name, result);
+        prettycli::info(&message);
+    }
+
+    pub fn balance_of(env: &HostEnv, owner: String) {
+        let owner = parse_address(&owner);
+        env.set_gas(1_000_000_000);
+        let result = contract(env).balance_of(owner);
+
+        let message = format!("Balance of {:?}: {}", owner, result);
+        prettycli::info(&message);
+    }
+
+    fn contract(env: &HostEnv) -> NameTokenHostRef {
+        let contracts = DeployedContracts::load(env);
+        contracts.token
+    }
+}
+
+pub mod resolver {
+    use super::*;
+
+    pub fn set_resolution(env: &HostEnv, full_domain: String, address: String) {
+        let addr = parse_address(&address);
+        assert!(addr.is_contract());
+
+        env.set_gas(10_000_000_000);
+        contract(env).set_resolution(full_domain, Some(addr));
+    }
+
+    pub fn cleanup(env: &HostEnv, token_name: String) {
+        env.set_gas(10_000_000_000);
+        contract(env).cleanup(token_name);
+    }
+
+    pub fn resolve(env: &HostEnv, domain: String) {
+        let result = contract(env).resolve(domain);
+
+        let result = result
+            .map(|addr| addr.to_string())
+            .unwrap_or_else(|| "Not found".to_string());
+        prettycli::info(&result);
+    }
+
+    fn contract(env: &HostEnv) -> DefaultResolverHostRef {
+        let contracts = DeployedContracts::load(env);
+        contracts.resolver
+    }
+}
+
+pub(super) fn now() -> u64 {
+    chrono::Utc::now().timestamp_millis() as u64
+}
+
+pub(super) fn blake2b<T: AsRef<[u8]>>(data: T) -> String {
+    let mut result = [0u8; 32];
+    let mut hasher = <Blake2bVar as VariableOutput>::new(32).expect("should create hasher");
+    let _ = hasher.write(data.as_ref());
+    hasher
+        .finalize_variable(&mut result)
+        .expect("should copy hash to the result array");
+    hex::encode(result)
+}
+
+pub(super) fn parse_address(addr: &str) -> Address {
+    let public_key = PublicKey::from_hex(addr.as_bytes());
+    match public_key {
+        Ok(public_key) => Address::from(public_key),
+        Err(_) => Address::from_str(addr).expect("Invalid address"),
+    }
 }
--- casper-name-cli/src/cli.rs
+++ casper-name-cli/src/cli.rs
@@ -21,45 +21,182 @@ enum Commands {
 
     #[clap(name = "controller", about = "Use the controller contract.")]
     Controller(Controller),
+
+    #[clap(name = "token", about = "Use the token contract.")]
+    NameToken(NameToken),
+
+    #[clap(name = "resolver", about = "Use the resolver contract.")]
+    Resolver(Resolver),
 }
 
 #[derive(Debug, Parser)]
 pub struct Registrar {
     #[structopt(subcommand)]
-    pub registrar_commands: RegistrarCommands,
+    pub cmds: RegistrarCommands,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum RegistrarCommands {
-    Register { name: String, buyer: String },
+    Register {
+        name: String,
+        buyer: String,
+        token_validity_in_seconds: Option<u64>,
+    },
+    Prolong {
+        name: String,
+        token_validity_in_seconds: Option<u64>,
+    },
+    SetDefaultResolver {
+        resolver: String,
+    },
+    Resolve {
+        name: String,
+    },
+    SetGracePeriod {
+        seconds: u64,
+    },
 }
 
 #[derive(Debug, Parser)]
 pub struct Controller {
     #[structopt(subcommand)]
-    pub controller_commands: ControllerCommands,
+    pub cmds: ControllerCommands,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum ControllerCommands {
-    Buy { name: String },
+    Buy {
+        name: String,
+        token_validity_in_seconds: Option<u64>,
+    },
+    Renew {
+        name: String,
+        token_validity_in_seconds: Option<u64>,
+    },
+    Resolve {
+        domain: String,
+    },
+}
+
+#[derive(Debug, Parser)]
+pub struct NameToken {
+    #[structopt(subcommand)]
+    pub cmds: NameTokenCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum NameTokenCommands {
+    SetResolver {
+        token_name: String,
+        resolver: String,
+    },
+    Resolver {
+        token_name: String,
+    },
+    Metadata {
+        token_name: String,
+    },
+    BalanceOf {
+        address: String,
+    },
+}
+
+#[derive(Debug, Parser)]
+pub struct Resolver {
+    #[structopt(subcommand)]
+    pub cmds: ResolverCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ResolverCommands {
+    Cleanup {
+        token_name: String,
+    },
+    SetResolution {
+        full_domain: String,
+        address: String,
+    },
+    Resolve {
+        full_domain: String,
+    },
 }
 
 pub fn parse() {
+    let env = odra_casper_livenet_env::env();
     match Cli::parse().command {
-        Commands::DeployContracts => actions::deploy_all(),
-        Commands::SetConfig => actions::set_config(),
-        Commands::Registrar(registrar) => match registrar.registrar_commands {
-            RegistrarCommands::Register { name, buyer } => {
-                actions::registrar_register(&name, &buyer)
+        Commands::DeployContracts => actions::deploy_all(&env),
+        Commands::SetConfig => actions::set_config(&env),
+        Commands::Registrar(registrar) => match registrar.cmds {
+            RegistrarCommands::Register {
+                name,
+                buyer,
+                token_validity_in_seconds,
+            } => {
+                let token_validity = to_mills(token_validity_in_seconds);
+                actions::registrar::register(&env, name, buyer, token_validity)
+            }
+            RegistrarCommands::Prolong {
+                name,
+                token_validity_in_seconds,
+            } => {
+                let token_validity = to_mills(token_validity_in_seconds);
+                actions::registrar::prolong(&env, name, token_validity)
+            }
+            RegistrarCommands::SetDefaultResolver { resolver } => {
+                actions::registrar::set_default_resolver(&env, resolver)
+            }
+            RegistrarCommands::Resolve { name } => actions::registrar::resolve(&env, name),
+            RegistrarCommands::SetGracePeriod { seconds } => {
+                actions::registrar::set_grace_period(&env, seconds)
             }
         },
-        Commands::Controller(controller) => match controller.controller_commands {
-            ControllerCommands::Buy { name } => actions::controller_buy(&name),
+        Commands::Controller(controller) => match controller.cmds {
+            ControllerCommands::Buy {
+                name,
+                token_validity_in_seconds,
+            } => {
+                let token_validity = to_mills(token_validity_in_seconds);
+                actions::controller::buy(&env, name, token_validity)
+            }
+            ControllerCommands::Renew {
+                name,
+                token_validity_in_seconds,
+            } => {
+                let token_validity = to_mills(token_validity_in_seconds);
+                actions::controller::renew(&env, name, token_validity)
+            }
+            ControllerCommands::Resolve { domain } => actions::controller::resolve(&env, domain),
+        },
+        Commands::NameToken(token) => match token.cmds {
+            NameTokenCommands::SetResolver {
+                token_name,
+                resolver,
+            } => actions::token::set_resolver(&env, token_name, resolver),
+            NameTokenCommands::Resolver { token_name } => {
+                actions::token::resolver(&env, token_name)
+            }
+            NameTokenCommands::Metadata { token_name } => {
+                actions::token::metadata(&env, token_name)
+            }
+            NameTokenCommands::BalanceOf { address } => actions::token::balance_of(&env, address),
+        },
+        Commands::Resolver(resolver) => match resolver.cmds {
+            ResolverCommands::Cleanup { token_name } => {
+                actions::resolver::cleanup(&env, token_name)
+            }
+            ResolverCommands::SetResolution {
+                full_domain,
+                address,
+            } => actions::resolver::set_resolution(&env, full_domain, address),
+            ResolverCommands::Resolve { full_domain } => {
+                actions::resolver::resolve(&env, full_domain)
+            }
         },
     }
 }
 
-fn _duration(seconds: Option<u64>) -> Option<std::time::Duration> {
-    seconds.map(|s| std::time::Duration::from_secs(s))
+fn to_mills(seconds: Option<u64>) -> Option<u64> {
+    seconds
+        .map(|s| std::time::Duration::from_secs(s))
+        .map(|d| d.as_millis() as u64)
 }

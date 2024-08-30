4cd49af49bc4107841e3622e26871252c83c55f1
Volodymyr-K <vladimir.kuchinskiy@make.services>
2024-08-30 14:58:36 +0200

    Adjust the code with examples

--- client-js/examples/config.ts
+++ client-js/examples/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   adminPrivateKeyPath: process.env.ADMIN_PRIVATE_KEY_PAIR_PATH,
+  registrarContractHash: process.env.REGISTRAR_CONTRACT_HASH,
   networkName: process.env.NETWORK_NAME,
   controllerContractHash: process.env.CONTROLLER_CONTRACT_HASH,
-  registrarContractHash: process.env.REGISTRAR_CONTRACT_HASH,
   nodeAddress: process.env.NODE_ADDRESS,
 };
